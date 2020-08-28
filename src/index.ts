2f3f5787eecb7179cb44b7d333bd1e177424c6bc
Davejs136 <davejs136@gmail.com>
2020-08-28 12:35:04 -0400

    Add: README.md

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import { println, print } from './print';
+export { println, print } from './print';
 
-const msg = "finally doit";
 
-// Showing basic colors
-println`{cyan}This is my other message{reset}`;
-println`{green}success: {magenta}Intl-graphl{reset}`;
+// // Showing basic colors
+// println`{cyan}This is my other message{reset}`;
+// println`{green}success: {magenta}Intl-graphl{reset}`;
 
-// TODO: Interpolation support
-const lang: string = 'en';
+// // TODO: Interpolation support
+// const lang: string = 'en';
 
-println`{green}sucess: {white}plugin intl-graphql for {red}${2+2}{white} languages {yellow}"${lang}"{reset}`;
+// println`{green}sucess: {white}plugin intl-graphql for {red}${2+2}{white} languages {yellow}"${lang}"{reset}`;
 
-console.log("");
+// console.log("");
