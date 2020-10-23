80e9090463634cbd53369e706c55417108d23dd7
Davejs136 <49698182+Davejs136@users.noreply.github.com>
2020-10-23 18:02:09 -0400

    refactor | code && delete comment lines (#4)
    ignore lock files
    
    add: clean command
    
    remove innecesary lines
    
    remove comment lines
    
    remove innecesary code
    
    add: transform.ts | allow to transform strings with colors defined
    
    delete: helpers.ts && yarn.lock

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,1 @@
 export { println, print } from './print';
-
-
-// // Showing basic colors
-// println`{cyan}This is my other message{reset}`;
-// println`{green}success: {magenta}Intl-graphl{reset}`;
-
-// // TODO: Interpolation support
-// const lang: string = 'en';
-
-// println`{green}sucess: {white}plugin intl-graphql for {red}${2+2}{white} languages {yellow}"${lang}"{reset}`;
-
-// console.log("");
